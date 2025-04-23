1d73100baaa4dd5153c8d4a1ae6967763c25a97f
Wanekeya <anythingpaula@gmail.com>
2025-04-23 09:34:56 +0200

    Update DomesticBusTrip.java

--- AbstractAndInterface/src/package1/DomesticBusTrip.java
+++ AbstractAndInterface/src/package1/DomesticBusTrip.java
@@ -1,22 +1,23 @@
 package package1;
 
 public class DomesticBusTrip extends TravelPlan implements Passenger {
-	private String departure;
-	private String destination;
-	private String travelDate;
 	
-	public DomesticBusTrip(String departure,String destination, String travelDate) {
+	
+	public DomesticBusTrip(String departure, String destination, String travelDate) {
 		super(departure,destination,travelDate);
-		this.departure = departure;
-		this.destination = destination;
-		this.travelDate = travelDate;
+		
 	}
 	
-	public void displayPassengerInfo(String name, String ticketNumber) {
-		System.out.println("The passenger's name is: " + name + ". The ticket number is: " + ticketNumber);
-	}
-
+	@Override
 	public void showTravelDetails() {
+		System.out.println("Domestic Bus Trip");
 		System.out.println("The departure date is: " + departure + ". The destination is: " + destination + ". The date of travel is: " + travelDate);	
 	}
+	
+	@Override
+	public void displayPassengerInfo(String name, String idNumber) {
+		System.out.println("The passenger's name is: " + name + ". The ID number is: " + idNumber);
+	}
+
+	
 }
