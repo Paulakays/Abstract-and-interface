a9d9368c95ac9522bec78b370ac87662c3ee9881
Wanekeya <anythingpaula@gmail.com>
2025-04-23 09:36:35 +0200

    Update InternationalBusTrip.java

--- AbstractAndInterface/src/package1/InternationalBusTrip.java
+++ AbstractAndInterface/src/package1/InternationalBusTrip.java
@@ -1,22 +1,21 @@
 package package1;
 
 public class InternationalBusTrip extends TravelPlan implements Passenger{
-	private String departure;
-	private String destination;
-	private String travelDate;
+	
 	
 	public InternationalBusTrip(String departure,String destination, String travelDate) {
 		super(departure,destination,travelDate);
-		this.departure = departure;
-		this.destination = destination;
-		this.travelDate = travelDate;
+		
 	}
-	
-	public void displayPassengerInfo(String name, String ticketNumber) {
-		System.out.println("Passenger name: " + name + ". Ticket  number: " + ticketNumber);
-	}
-	
+	@Override
 	public void showTravelDetails() {
+		System.out.println("International Bus Trip");
 		System.out.println("Departure date: " + departure + ". Destination: " + destination + ". Date of travel: " + travelDate);	
 	}
+	@Override 
+	public void displayPassengerInfo(String name, String passportNumber) {
+		System.out.println("Passenger name: " + name + ". Passport  number: " + passportNumber);
+	}
+	
+	
 }
